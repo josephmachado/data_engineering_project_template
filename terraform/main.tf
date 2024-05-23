c6e0e9ade4f09c3f48214bc2e3a2a1f1bb24bc23
sardorboboshov <sardorboboshov@gmail.com>
2024-05-24 00:42:28 +0500

    updating deprecated ubuntu ami version

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220420"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240514"]
   }
 
   filter {

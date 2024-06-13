3ba0edf25a1004f83351f3b5a8d75200b75d1514
JosephKevinMachado <josephkevinmachado@pop-os.fios-router.home>
2024-06-13 13:38:58 -0400

    2024-06-13-13-38-58 - add-tests

--- dags/coincap_elt.py
+++ dags/coincap_elt.py
@@ -7,6 +7,9 @@ import requests
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+from cuallee import Check, CheckLevel
+import polars as pl
+from airflow.operators.dummy import DummyOperator
 
 with DAG(
     'coincap_elt',
@@ -32,6 +35,23 @@ with DAG(
                 dict_writer.writeheader()
                 dict_writer.writerows(exchanges)
 
+    def check_completeness(pl_df, column_name):
+        check = Check(CheckLevel.ERROR, "Completeness")
+        validation_results_df = (
+            check.is_complete(column_name).validate(pl_df)
+        )
+        return validation_results_df["status"].to_list()
+    
+    @task.branch
+    def check_data_quality(validation_results):
+        if "FAIL" not in validation_results:
+            return ['generate_dashboard']
+        return ['stop_pipeline']
+    
+    check_data_quality_instance = check_data_quality(check_completeness(pl.read_csv(file_path), "name"))
+
+    stop_pipeline = DummyOperator(task_id='stop_pipeline')
+
     markdown_path = f'{os.getenv("AIRFLOW_HOME")}/visualization/'
     q_cmd = (
         f'cd {markdown_path} && quarto render {markdown_path}/dashboard.qmd'
@@ -40,4 +60,5 @@ with DAG(
         task_id="generate_dashboard", bash_command=q_cmd
     )
 
-    fetch_coincap_exchanges(url, file_path) >> gen_dashboard
+    fetch_coincap_exchanges(url, file_path) >> check_data_quality_instance >> gen_dashboard
+    fetch_coincap_exchanges(url, file_path) >> check_data_quality_instance >> stop_pipeline
